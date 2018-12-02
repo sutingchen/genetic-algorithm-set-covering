e15c7cca2c18d3bb76d42e6a43dc00da0b992cd1
Su-Ting Chen <wendy10935@hotmail.com>
2018-12-02 10:45:54 +0800

    defined problem class

--- SCP.GeneticAlgorithm.App/Main.cpp
+++ SCP.GeneticAlgorithm.App/Main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include "Contants.h"
+#include "Problem.h"
 
+using namespace std;
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	Problem problem;
+	problem.ReadFile(FILE_DIRECTORY);
+
+#pragma region print columns and rows count
+	cout << "columns_count: " << problem.columns_count() << endl;
+	cout << "rows_count: " << problem.rows_count() << endl;
+#pragma endregion
+
+#pragma region print costs
+	vector<int>::iterator it_costs;
+
+	cout << "printing costs:" << endl;
+	vector<int> costs = problem.costs();
+	for (it_costs = costs.begin(); it_costs != costs.end(); ++it_costs)
+	{
+		std::cout << *it_costs << " ";
+	}
+	cout << endl;
+#pragma endregion
+
+#pragma region print row coverings
+	vector<int>::iterator it_row_coverings;
+
+	cout << "printing row_coverings:" << endl;
+
+	for (size_t i = 0; i < problem.row_coverings().size() - 1; i++) {
+		vector<int> single_row_covering = problem.row_coverings()[i];
+		for (it_row_coverings = single_row_covering.begin(); it_row_coverings != single_row_covering.end(); ++it_row_coverings)
+		{
+			std::cout << *it_row_coverings << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+#pragma endregion
+
+#pragma region print column coverings
+	vector<int>::iterator it_column_coverings;
+
+	cout << "printing column_coverings:" << endl;
+
+	for (size_t i = 0; i < problem.column_coverings().size() - 1; i++) {
+		vector<int> single_column_covering = problem.column_coverings()[i];
+		for (it_column_coverings = single_column_covering.begin(); it_column_coverings != single_column_covering.end(); ++it_column_coverings)
+		{
+			std::cout << *it_column_coverings << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+#pragma endregion
+
+
+
 	return 0;
 }
--- SCP.GeneticAlgorithm.App/Problem.h
+++ SCP.GeneticAlgorithm.App/Problem.h
@@ -1,60 +1,162 @@
+#include <boost/algorithm/string.hpp>
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Utilities.h"
 
 using namespace std;
 
 class Problem
 {
 private:
-	int rows;
-	int columns;
-	vector<int> costs;
-	vector<vector<int>> rowCoverings; // the set of columns that covers row
-	vector<vector<int>> columnCoverings; // the set of rows that covers column
+	int rows_count_;
+	int columns_count_;
+	vector<int> costs_;
+	vector<vector<int>> row_coverings_; // the set of columns that covers row
+	vector<vector<int>> column_coverings_; // the set of rows that covers column
 
 public:
-	int getRows() {
-		return rows;
+	int rows_count() {
+		return rows_count_;
 	}
 
-	void setRows(int rows) {
-		this->rows = rows;
+	void rows_count(int rows_count) {
+		this->rows_count_ = rows_count;
 	}
 
-	int getColumns() {
-		return columns;
+	int columns_count() {
+		return columns_count_;
 	}
 
-	void setColumns(int columns) {
-		this->columns = columns;
+	void columns_count(int columns_count) {
+		this->columns_count_ = columns_count;
 	}
 
-	vector<int> getCosts() {
-		return costs;
+	vector<int> costs() {
+		return costs_;
 	}
 
-	void setCosts(vector<int> costs) {
-		this->costs = costs;
+	void costs(vector<int> costs) {
+		this->costs_ = costs;
 	}
 
-	vector<vector<int>> getRowCoverings() {
-		return rowCoverings;
+	vector<vector<int>> row_coverings() {
+		return row_coverings_;
 	}
 
-	void setRowCoverings(vector<vector<int>> rowCoverings) {
-		this->rowCoverings = rowCoverings;
+	void row_coverings(vector<vector<int>> row_coverings) {
+		this->row_coverings_ = row_coverings;
 	}
 
-	vector<vector<int>> getColumnCoverings() {
-		return columnCoverings;
+	vector<vector<int>> column_coverings() {
+		return column_coverings_;
 	}
 
-	void setColumnCoverings(vector<vector<int>> columnCoverings) {
-		this->columnCoverings = columnCoverings;
+	void column_coverings(vector<vector<int>> column_coverings) {
+		this->column_coverings_ = column_coverings;
 	}
 
+
+	void ReadFile(string input_file) {
+
+		cout << "inputFile" << input_file << endl;
+
+		ifstream file(input_file);
+
+		if (file.is_open()) {
+		
+			cout << "file is opened" << endl;
+
+			string size;
+			vector<string> sizes;
+			vector<int> costs;
+			int counter = 0;
+
+			getline(file, size);
+			boost::algorithm::split(sizes, size, boost::is_any_of(" "));
+			rows_count(stoi(sizes[0]));
+			columns_count(stoi(sizes[1]));
+
+
+			// read input file and store the costs into private member "costs"
+			while (counter < columns_count_) {
+				string line; 
+				vector<string> temp_costs;
+
+				getline(file, line);
+				cout << "line: " << line << endl;
+				boost::trim(line);
+				boost::algorithm::split(temp_costs, line, boost::is_any_of(" "));
+				for (size_t i = 0; i < temp_costs.size(); i++) {
+					costs.push_back(stoi(temp_costs[i]));
+					counter++;
+				}
+			}
+
+			this->costs(costs);
+
+			// set row_coverings
+			for (int i = 0; i < this->rows_count(); i++) {
+				string line;
+				vector<string> str_num_of_columns;
+				getline(file, line);
+				boost::trim(line);
+				boost::algorithm::split(str_num_of_columns, line, boost::is_any_of(" "));
+
+				// number of columns in the current set
+				int num_of_columns = stoi(str_num_of_columns[0]);
+
+				
+				int col_counter = 0;
+				vector<int> single_row_coverings; //
+
+				// read the set of columns that cover each rows (alpha[i]), aka "row_covering"
+				while (col_counter < num_of_columns) {
+					vector<string> temp_columns;
+					getline(file, line);
+					boost::trim(line);
+					boost::algorithm::split(temp_columns, line, boost::is_any_of(" "));
+					
+					for (size_t j = 0; j < temp_columns.size(); j++) {
+						single_row_coverings.push_back(stoi(temp_columns[j]));
+						col_counter++;
+					}
+				}
+
+				//vector<vector<int>>::iterator it;
+				//this->row_coverings_.insert(row_coverings_.end(), single_row_coverings.begin(), single_row_coverings.end());
+
+				this->row_coverings_.push_back(single_row_coverings);
+			}
+
+
+			// set column_coverings
+			for (int i = 1; i < this->columns_count(); i++) {
+				vector<int> single_column_coverings;
+				for (int j = 1; j < this->rows_count(); j++) {
+					vector<int> single_row_coverings = this->row_coverings_[j - 1];
+
+					if (find(single_row_coverings.begin(), single_row_coverings.end(), i) != single_row_coverings.end()) {
+						/* single_row_coverings contains i */
+						single_column_coverings.push_back(j);
+					}
+				}
+				//vector<vector<int>>::iterator it;
+				//this->column_coverings_.insert(column_coverings_.end(), single_column_coverings.begin(), single_column_coverings.end());
+			
+				this->column_coverings_.push_back(single_column_coverings);
+			}
+
+			file.close();
+
+			cout << "file is closed" << endl;
+		}
+		else {
+
+			// TODO: throw exception
+			cout << "Unable to open file" << endl;
+		}
+
+	}
 
 };
